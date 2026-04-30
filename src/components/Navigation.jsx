e9454403c6b8c15cbc7f2b60a9e6ce0f013d9d0b
oualid-cpu <oualidchiki@gmail.com>
2026-04-30 11:16:37 +0200

    Fix navigation links for GitHub Pages

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,12 @@
+const basePath = import.meta.env.BASE_URL;
+
 function Navigation() {
     return (
         <nav className="site-nav">
-            <a href="/">Home</a>
-            <a href="/politics">Politics</a>
-            <a href="/technology">Technology</a>
-            <a href="/sports">Sports</a>
+            <a href={basePath}>Home</a>
+            <a href={`${basePath}politics`}>Politics</a>
+            <a href={`${basePath}technology`}>Technology</a>
+            <a href={`${basePath}sports`}>Sports</a>
         </nav>
     );
 }
